487f7bd48fdda9fb8dae2a97dbfe21bfafe4fc56
abhishekyadav4 <yadavabhishek487@gmail.com>
2024-08-25 11:47:22 +0530

    Ignore all files

--- src/AsyncHandler.js
+++
@@ -1,9 +0,0 @@
-
-const asyncHandler = (requesHandler)=>{
-    return  (req,res,next)=>{
-          Promise.resolve(requesHandler(req,res,next))
-          .catch((err)=>next(err))
-      }
-  }
-  
-  export {asyncHandler}
